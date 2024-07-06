c2f22a6ddddb00a7f3fd41479a1a3682c53e2ef6
prashantr2 <prashantrawat2ofc@gmail.com>
2024-07-06 19:50:13 +0530

    Added city-routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig.js');
-const { City } = require('./models/index');
+const { apiRoutes } = require('./routes/index.js');
 
 const setupAndStartServer = async () => {
     const app = express();
     
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    
+    app.use('/api', apiRoutes);
     
     app.listen(PORT, () => {
         console.log(`Server up and running on port ${PORT}`);
