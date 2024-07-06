65155eb53c5a96749e318a5d86d7014e96eb2279
prashantr2 <prashantrawat2ofc@gmail.com>
2024-07-06 18:55:14 +0530

    Completed city repository

--- src/repositories/city-repository.js
+++ src/repositories/city-repository.js
@@ -17,6 +17,29 @@ class CityRepository{
                     id: cityId
                 }
             }) 
+            return true;
+        } catch (error) {
+            throw { error };
+        }
+    }
+    
+    async updateCity(cityId, data) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            throw { error };
+        }
+    }
+    
+    async getCity(cityId) {
+        try {
+            const city = City.findByPk(cityId);
+            return city;
         } catch (error) {
             throw { error };
         }
